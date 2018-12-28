57c0fc335b2f39475a1a5c58eb012a34d9d9628f
Fangzhou Xu <fangzxu@gmail.com>
2018-12-28 13:08:43 -0800

    Change the results look & feel a little bit (#19)
    * change the results look
    
    * reorder the imports

--- app/src/components/entityDetails/EntityDetails.js
+++ app/src/components/entityDetails/EntityDetails.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 import PropTypes from 'prop-types';
 import ReactJson from 'react-json-view';
 
 import './EntityDetails.css';
 
+const styles = theme => ({
+  root: {
+    width: '100%',
+    overflowX: 'auto',
+  },
+});
+
 //This component ended up being a pretty thin wrapper around react-json-view
 //3rd party comp. But still valuable as a separate component since it will be
 //used in multiple routes/views of the app.
-export default class EntityDetails extends Component {
+class EntityDetails extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className="Details">
-        <ReactJson src={this.props.selectedObj} />
-      </div>
+      <Paper className={classes.root}>
+        <ReactJson src={this.props.selectedObj} theme="ocean"/>
+      </Paper>
     );
   }
 }
@@ -20,3 +31,5 @@ export default class EntityDetails extends Component {
 EntityDetails.propTypes = {
   selectedObj: PropTypes.object,
 };
+
+export default withStyles(styles)(EntityDetails);
