df795472fb668b589c84da2959b0f2893fc691e7
David Masselink <David_Masselink@intuit.com>
2019-01-31 22:28:24 -0800

    bugfix/reset-graph-data - fix bug; add new logos and remove old; improve hierarchy with programatic levels

--- app/src/components/home/Home.js
+++ app/src/components/home/Home.js
@@ -8,7 +8,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 import './Home.css';
-import logo from './map.png';
+import logo from './katlas-logo-blue-300px.png';
 import { ENTER_KEYCODE, ENTER_KEYSTR } from '../../config/appConfig';
 import * as queryActions from '../../actions/queryActions';
 import { validateQslQuery } from '../../utils/validate';
